9229182ed871caf34b8402a1e6916b6ca2f00229
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-03-15 21:25:28 +0300

    +PHP Array merge

--- test.php
+++ test.php
@@ -11,17 +11,109 @@
 <body>
     <header class="uni-header">
         <h1><a href="index.html">PyramidEX</a></h1>
-        <nav class="i-header-grid">
-            <a href="trading.html">TRADE</a>
-            <a href="wallet.html">WALLET</a>
-            <a href="profile.html">PROFILE</a>
-            <a href="contacts.html">CONTACTS</a>
-        </nav>
+        <form class="t-header-form" method="get">
+            <?php if ( isset($_GET['coloredMenu']) && $_GET['coloredMenu'] == "TRADE" ): ?>
+                <input type="submit" name="coloredMenu" value="TRADE" id="php-coloredBG">
+            <?php else: ?>
+                <input type="submit" name="coloredMenu" value="TRADE">
+            <?php endif ?>
+
+            <?php if ( isset($_GET['coloredMenu']) && $_GET['coloredMenu'] == "WALLET" ): ?>
+                <input type="submit" name="coloredMenu" value="WALLET" id="php-coloredBG">
+            <?php else: ?>
+                <input type="submit" name="coloredMenu" value="WALLET">
+            <?php endif ?>
+
+            <?php if ( isset($_GET['coloredMenu']) && $_GET['coloredMenu'] == "PROFILE" ): ?>
+                <input type="submit" name="coloredMenu" value="PROFILE" id="php-coloredBG">
+            <?php else: ?>
+                <input type="submit" name="coloredMenu" value="PROFILE">
+            <?php endif ?>
+
+            <?php if ( isset($_GET['coloredMenu']) && $_GET['coloredMenu'] == "CONTACTS" ): ?>
+                <input type="submit" name="coloredMenu" value="CONTACTS" id="php-coloredBG">
+            <?php else: ?>
+                <input type="submit" name="coloredMenu" value="CONTACTS">
+            <?php endif ?>
+        </form>
     </header>
-    <main class="i-main-wrapper">
-        <?php
-            echo 'PHP';
-        ?>
+    <main class="t-main-wrapper">
+        <form class="uni-form" method="get">
+            <h1>Merge arrays</h1>
+            <h2>Input numbers separated with spaces</h2>
+            <?php
+                function arr_concat($arr1, $arr2){
+                    $buffLength = count($arr1) + count($arr2);
+                    $firstI = count($arr1);
+                    for ( $i = count($arr1); $i < $buffLength; $i++ ){
+                        $arr1[$i] = $arr2[$i - $firstI];
+                    }
+                    return $arr1;
+                }
+
+                function arr_even($arr){
+                    $buffArray = array();
+                    $j = 0;
+                    for ( $i = 0; $i < count($arr); $i++ ){
+                        if ( $arr[$i] % 2 == 0 ){
+                            $buffArray[$j] = $arr[$i];
+                            $j++;
+                        }
+                    }
+                    return $buffArray;
+                }
+
+                if ( isset($_GET['mrg_arr1']) ) {
+                    echo '<p>';
+                    echo '1st array: ';
+                    if ( strlen($_GET['mrg_arr1']) > 0 ) {
+                        echo '[' . $_GET['mrg_arr1'] . ']' . '<br>';
+                    } else {
+                        echo ' []' . '<br>';
+                    }
+                    echo '</p>';
+                }
+
+                if ( isset($_GET['mrg_arr2']) ) {
+                    echo '<p>';
+                    echo '2nd array: ';
+                    if ( strlen($_GET['mrg_arr2']) > 0 ) {
+                        echo '[' . $_GET['mrg_arr2'] . ']' . '<br/>';
+                    } else {
+                        echo ' []' . '<br>';
+                    }
+                    echo '</p>';
+                }
+
+                if ( isset($_GET['mrg_arr1']) && isset($_GET['mrg_arr2']) ) {
+                    $numArray1 = array();
+                    $numArray2 = array();
+
+                    if ( strlen($_GET['mrg_arr1']) > 0 ){
+                        $numArray1 = explode(" ",$_GET['mrg_arr1']);
+                    }
+
+                    if ( strlen($_GET['mrg_arr2']) > 0 ){
+                        $numArray2 = explode(" ",$_GET['mrg_arr2']);
+                    }
+
+                    $numArrayMrg = arr_concat($numArray1,$numArray2);
+                    $numArrayMrgEven = arr_even($numArrayMrg);
+
+                    echo '<p>';
+                    echo 'New array: [' . implode( " ", $numArrayMrg) . ']';
+                    echo '</p>';
+
+                    echo '<p>';
+                    echo 'Even array: [' . implode( " ", $numArrayMrgEven) . ']';
+                    echo '</p>';
+                }
+            ?>
+
+            <input type="text" autocomplete="off" name="mrg_arr1" placeholder="1st array" pattern="^([0-9] ?)*[0-9]$">
+            <input type="text" autocomplete="off" name="mrg_arr2" placeholder="2nd array" pattern="^([0-9] ?)*[0-9]$">
+            <input type="submit" name="" value="Merge">
+        </form>
     </main>
     <footer class="uni-footer">
         <a href="test.php">&copy PyEX Digital Assets, Ltd. 2020 - Seychelles</a>
