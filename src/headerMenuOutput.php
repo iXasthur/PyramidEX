832f6bf68254e6efa7c378f3c25b5e2adbba5b9b
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2020-04-12 03:21:10 +0300

    Changed POST to GET for header

--- src/headerMenuOutput.php
+++ src/headerMenuOutput.php
@@ -1,26 +1,26 @@
 <form class="uni-header-nav-form" method="post" action="trading.php">
-    <?php if (isset($_POST['coloredMenu']) && 'TRADE' == $_POST['coloredMenu']) { ?>
+    <?php if (isset($_GET['coloredMenu']) && 'TRADE' == $_GET['coloredMenu']) { ?>
         <input type="submit" name="coloredMenu" value="TRADE" id="php-coloredBG">
     <?php } else { ?>
         <input type="submit" name="coloredMenu" value="TRADE">
     <?php } ?>
 </form>
 <form class="uni-header-nav-form" method="post" action="wallet.php">
-    <?php if (isset($_POST['coloredMenu']) && 'WALLET' == $_POST['coloredMenu']) { ?>
+    <?php if (isset($_GET['coloredMenu']) && 'WALLET' == $_GET['coloredMenu']) { ?>
         <input type="submit" name="coloredMenu" value="WALLET" id="php-coloredBG">
     <?php } else { ?>
         <input type="submit" name="coloredMenu" value="WALLET">
     <?php } ?>
 </form>
 <form class="uni-header-nav-form" method="post" action="profile.php">
-    <?php if (isset($_POST['coloredMenu']) && 'PROFILE' == $_POST['coloredMenu']) { ?>
+    <?php if (isset($_GET['coloredMenu']) && 'PROFILE' == $_GET['coloredMenu']) { ?>
         <input type="submit" name="coloredMenu" value="PROFILE" id="php-coloredBG">
     <?php } else { ?>
         <input type="submit" name="coloredMenu" value="PROFILE">
     <?php } ?>
 </form>
 <form class="uni-header-nav-form" method="post" action="contacts.php">
-    <?php if (isset($_POST['coloredMenu']) && 'CONTACTS' == $_POST['coloredMenu']) { ?>
+    <?php if (isset($_GET['coloredMenu']) && 'CONTACTS' == $_GET['coloredMenu']) { ?>
         <input type="submit" name="coloredMenu" value="CONTACTS" id="php-coloredBG">
     <?php } else { ?>
         <input type="submit" name="coloredMenu" value="CONTACTS">
